7cd19d6a02415e32a46aaf8d453b15d53c80bc6f
mosuke5 <w.vamos603@gmail.com>
2016-03-28 14:18:49 +0900

    activity情報を詳細に表示

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,6 +1,26 @@
 class PlayerController < ApplicationController
   def show
+    year = 2016
     @player = Player.convert_name_from_url_name(params[:player_url_name])
     @activities = Activity.where("player_name = ?", @player)
+    @activities_vstop10 = Activity
+      .where("opponent_rank <= ?", 10)
+      .where("player_name = ?", @player)
+      .where("year = ?", year)
+      .order("tournament_start_date")
+      .order("id DESC")
+    @activities_higher = Activity
+      .where("player_rank > opponent_rank")
+      .where("player_name = ?", @player)
+      .where("year = ?", year)
+      .order("tournament_start_date")
+      .order("id DESC")
+    @activities_lower = Activity
+      .where("player_rank < opponent_rank")
+      .where("win_loss = ?", "L")
+      .where("player_name = ?", @player)
+      .where("year = ?", year)
+      .order("tournament_start_date")
+      .order("id DESC")
   end
 end
