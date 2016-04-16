9ba2df3bf39bb47c552c482571b48e604d0441cb
mosuke5 <w.vamos603@gmail.com>
2016-04-16 18:16:20 +0900

    アクティビティのall表示を修正

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -3,7 +3,9 @@ class PlayerController < ApplicationController
     @year = Date.today.year.to_s
     @year = params[:year].to_s if params[:year]
     @player = Player.convert_name_from_url_name(params[:player_url_name])
-    @activities = Activity.where("player_name = ?", @player)
+    @activities = Activity
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
     @activities_vstop10 = Activity
       .where("opponent_rank <= ?", 10)
       .where("player_name = ?", @player)
--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-port        3000
-environment 'development'
+port        ENV['ATPSTAT_PORT'] || 3000
+environment ENV['ATPSTAT_RACK_ENV'] || 'development'
 daemonize   true
 pidfile     'atp-stat.pid'
