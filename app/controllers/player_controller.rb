8c95e6490846e2bece0a4bcb71288e84a72b59e9
mosuke5 <w.vamos603@gmail.com>
2016-05-03 23:02:55 +0900

    勝敗数表示を追加

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -6,24 +6,69 @@ class PlayerController < ApplicationController
     @activities = Activity
       .where("player_name = ?", @player)
       .where("year = ?", @year)
+    @activities_all_win_count = Activity
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "W")
+      .count
+    @activities_all_lose_count = Activity
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "L")
+      .count
     @activities_vstop10 = Activity
       .where("opponent_rank <= ?", 10)
       .where("player_name = ?", @player)
       .where("year = ?", @year)
       .order("tournament_start_date")
       .order("id DESC")
+    @activities_vstop10_win_count = Activity
+      .where("opponent_rank <= ?", 10)
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "W")
+      .count
+    @activities_vstop10_lose_count = Activity
+      .where("opponent_rank <= ?", 10)
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "L")
+      .count
     @activities_higher = Activity
       .where("player_rank > opponent_rank")
       .where("player_name = ?", @player)
       .where("year = ?", @year)
       .order("tournament_start_date")
       .order("id DESC")
+    @activities_higher_win_count = Activity
+      .where("player_rank > opponent_rank")
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "W")
+      .count
+    @activities_higher_lose_count = Activity
+      .where("player_rank > opponent_rank")
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "L")
+      .count
     @activities_lower = Activity
       .where("player_rank < opponent_rank")
-      .where("win_loss = ?", "L")
       .where("player_name = ?", @player)
       .where("year = ?", @year)
       .order("tournament_start_date")
       .order("id DESC")
+    @activities_lower_win_count = Activity
+      .where("player_rank < opponent_rank")
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "W")
+      .count
+    @activities_lower_lose_count = Activity
+      .where("player_rank < opponent_rank")
+      .where("player_name = ?", @player)
+      .where("year = ?", @year)
+      .where("win_loss = ?", "L")
+      .count
   end
 end
