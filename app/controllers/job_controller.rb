43618bd1e05ea3b6b9fcb176b64d7948d4b50fb1
mosuke5 <w.vamos603@gmail.com>
2016-06-12 15:36:58 +0900

    jobページ改良

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -2,4 +2,14 @@ class JobController < ApplicationController
   def index
     @jobs = ActivityJob.all.order("id")
   end
+  
+  def show
+    @job = ActivityJob.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
 end
