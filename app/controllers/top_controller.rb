009df5cd1263b3f516f13273ad07947bdd5a6cb1
mosuke5 <w.vamos603@gmail.com>
2016-08-11 04:42:11 +0000

    現在の分析対象数を表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,8 @@
 class TopController < ApplicationController
   def index
     @players = Player.all
+    @players_count = Player.all.count
+    @activities_count = Activity.all.count
     @ranking_vs_top10  = PlayerStatus.ranking_vs_top10
     @ranking_vs_higher = PlayerStatus.ranking_vs_higher
     @ranking_vs_lower  = PlayerStatus.ranking_vs_lower
